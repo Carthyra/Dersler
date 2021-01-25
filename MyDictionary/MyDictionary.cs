f657b2c0aab30f2e3acae667470f2a5722b5cc4f
Carthyra <ulascumart@gmail.com>
2021-01-25 23:26:01 +0300

    Odev Dictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MyDictionary
+{
+    public class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[tempKeys.Length+1];
+            values = new V[tempValues.Length+1];
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length-1] = key;
+            values[values.Length-1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ulaş");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add(2, "Gizem");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add(3, "Bıdık");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add(4, "Pofuduk");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
